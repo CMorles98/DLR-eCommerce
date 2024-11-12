f01cb821030cc2af438e7f0b95521779cb354a9f
Christopher Morles <80721516+CMorles98@users.noreply.github.com>
2024-11-12 19:37:21 -0400

    add new route to prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true, 
-    baseUrl: 'https://dlr-api.up.railway.app/api',
+    baseUrl: 'https://dlr-e-commerce-api.vercel.app/api',
     apiKey: 'AIzaSyCs4ZH8FlBcxkHD6nLjDQBD5z07xaDRO2A'
 };
